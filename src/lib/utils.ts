89923a74b5f1e79861706d482a40183ff7a9f061
MuhammedKasujja <al.kasmud.2@gmail.com>
2026-09-15 11:43:51 +0300

    chores: migrate cn old function to new shadcn cn utility function

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,10 +1,5 @@
 import { DateTime } from "luxon";
-import { twMerge } from "tailwind-merge"
-import { clsx, type ClassValue } from "clsx"
-
-export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
-}
+export { cn } from "cn";
 
 export function getDateTime() {
   return DateTime.now();
