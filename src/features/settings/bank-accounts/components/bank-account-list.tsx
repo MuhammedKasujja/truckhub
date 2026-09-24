8e4e623a8cd8f80296bbe9e2441561566cf2b99a
MuhammedKasujja <al.kasmud.2@gmail.com>
2026-09-24 14:25:05 +0300

    feat: bank account edit form

--- src/features/settings/bank-accounts/components/bank-account-list.tsx
+++ src/features/settings/bank-accounts/components/bank-account-list.tsx
@@ -10,6 +10,8 @@ import { BankAccount } from "../types"
 import { Button } from "@/components/ui/button"
 import { ActionIcon } from "@/components/icons"
 import { Badge } from "@/components/ui/badge"
+import { useState } from "react"
+import { BankAccountForm } from "./bank-account-form"
 
 type Props = {
   accounts: BankAccount[]
@@ -18,32 +20,51 @@ type Props = {
 export function BankAccountList({ accounts }: Props) {
   return (
     <div className="space-y-4">
-      {accounts.map((acc) => (
-        <Card>
-          <CardHeader>
-            <CardTitle>{acc.purpose}</CardTitle>
-            <CardDescription>{acc.bank.name}</CardDescription>
-            <CardAction>
-              <Badge variant={acc.is_active ? "default" : "destructive"}>
-                {acc.is_active ? "Active" : "Inactive"}
-              </Badge>
-            </CardAction>
-          </CardHeader>
-          <CardFooter className="flex justify-between">
-            <div>
-              .... {acc.account_number.slice(-4)} {acc.currency}
-            </div>
-            <div className="flex gap-2">
-              <Button size={"icon-sm"} variant={"outline"}>
-                <ActionIcon action="view" />
-              </Button>
-              <Button size={"icon-sm"} variant={"outline"}>
-                <ActionIcon action="edit" />
-              </Button>
-            </div>
-          </CardFooter>
-        </Card>
+      {accounts.map((account) => (
+        <BankAccountListItem key={account.id} account={account} />
       ))}
     </div>
+  )
+}
+
+function BankAccountListItem({ account }: { account: BankAccount }) {
+  const [open, setOpen] = useState<"bank" | "account">()
+  return (
+    <>
+      <Card>
+        <CardHeader>
+          <CardTitle>{account.purpose}</CardTitle>
+          <CardDescription>{account.bank.name}</CardDescription>
+          <CardAction>
+            <Badge variant={account.is_active ? "default" : "destructive"}>
+              {account.is_active ? "Active" : "Inactive"}
+            </Badge>
+          </CardAction>
+        </CardHeader>
+        <CardFooter className="flex justify-between">
+          <div>
+            .... {account.account_number.slice(-4)} {account.currency}
+          </div>
+          <div className="flex gap-2">
+            <Button size={"icon-sm"} variant={"outline"}>
+              <ActionIcon action="view" />
+            </Button>
+            <Button
+              size={"icon-sm"}
+              variant={"outline"}
+              onClick={() => setOpen("account")}
+            >
+              <ActionIcon action="edit" />
+            </Button>
+          </div>
+        </CardFooter>
+      </Card>
+      <BankAccountForm
+        key={account.id}
+        initialData={account}
+        open={open == "account"}
+        onOpenChange={() => setOpen(undefined)}
+      />
+    </>
   )
 }
